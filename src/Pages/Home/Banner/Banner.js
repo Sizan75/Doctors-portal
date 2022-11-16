92fe5d29bf068cdebb3d057f6b9aaf1a06f909f8
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-16 09:55:31 +0600

    Navber complete

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Banner = () => {
     return (
-        <div className='mx-5'>
+        <div>
             
         </div>
     );
 };
 
-export default Home;
+export default Banner;
