d6cfb85cff96f84559c7b4dbfcf6026161a43279
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-20 14:09:34 +0600

    react query added

--- src/Pages/Appoinment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appoinment/AvailableAppointment/AvailableAppointment.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import { format } from 'date-fns';
+import {useQuery} from '@tanstack/react-query'
 import AppointmentOption from './AppointmentOption';
 import BookingModal from '../BookingModal/BookingModal';
 const AvailableAppointment = ({selectedDate}) => {
-    const [appointmentOptions, setAppointmentOptions] = useState([]);
     const [treatment, setTreatment] = useState(null);
-    useEffect(() => {
-        fetch('AppointmentOptions.json')
-            .then(res => res.json())
-            .then(data => setAppointmentOptions(data))
-    }, [])
+    
+    const {data:appointmentOptions=[]}= useQuery({
+        queryKey:['appointmentOptions'],
+        queryFn: ()=> fetch('http://localhost:5000/appointmentoptions')
+        .then(res => res.json())
+    })
+
     return (
         <section className='my-16'>
             <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
